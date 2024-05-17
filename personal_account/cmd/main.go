be869f8baae4baa1e2d3030cf45ea63c512672fa
SashaMelva <sa1524517@gmail.com>
2024-05-17 14:46:36 +0300

    accounts add

---
+++ personal_account/cmd/main.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"context"
+	"os/signal"
+	"syscall"
+	"time"
+
+	"github.com/SashaMelva/smart_filter/internal/app"
+	"github.com/SashaMelva/smart_filter/internal/config"
+	"github.com/SashaMelva/smart_filter/internal/logger"
+	"github.com/SashaMelva/smart_filter/internal/memory/connection"
+	storage "github.com/SashaMelva/smart_filter/internal/memory/storage/postgre"
+	"github.com/SashaMelva/smart_filter/internal/server/http"
+)
+
+func main() {
+	configFile := "../configs/"
+	config := config.New(configFile)
+	log := logger.New(config.Logger, "../logs/")
+
+	connectionDB := connection.New(config.DataBase, log)
+	// err := migrator.RunMigrationsPg(connectionDB, "migrations")
+
+	// if err != nil {
+	// 	log.Fatal(err)
+	// }
+
+	memstorage := storage.New(connectionDB.StorageDb, log)
+	app := app.New(log, memstorage)
+
+	httpServer := http.NewServer(log, app, config.HttpServer)
+
+	ctx, cancel := signal.NotifyContext(context.Background(),
+		syscall.SIGINT, syscall.SIGTERM, syscall.SIGHUP)
+	defer cancel()
+
+	go func() {
+		<-ctx.Done()
+
+		ctx, cancel := context.WithTimeout(context.Background(), time.Second*3)
+		defer cancel()
+
+		httpServer.Stop(ctx)
+	}()
+
+	log.Info("Services is running...")
+	log.Debug("Debug mode enabled")
+
+	httpServer.Start(ctx)
+}
---
+++ personal_account/internal/server/http/server.go
@@ -0,0 +1,52 @@
+package http
+
+import (
+	"context"
+	"fmt"
+	"net/http"
+	"os"
+
+	"github.com/SashaMelva/smart_filter/internal/app"
+	"github.com/SashaMelva/smart_filter/internal/config"
+	"github.com/gin-gonic/gin"
+	"go.uber.org/zap"
+)
+
+type Server struct {
+	srv *http.Server
+	log *zap.SugaredLogger
+}
+
+func NewServer(log *zap.SugaredLogger, app *app.App, config *config.ConfigHttpServer) *Server {
+	log.Debug("URL: " + config.Host + ":" + config.Port)
+	router := gin.Default()
+	// handler := httphandler.NewHendler(log, app)
+
+	router.GET("/", func(ctx *gin.Context) {
+		fmt.Println("Hellow world)")
+	})
+
+	// router.POST("/event/", handler.)
+
+	return &Server{
+		srv: &http.Server{
+			Addr:    config.Host + ":" + config.Port,
+			Handler: router,
+		},
+		log: log,
+	}
+}
+
+func (s *Server) Start(ctx context.Context) {
+	if err := s.srv.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+		s.log.Fatalf("listen: %s\n", err)
+	}
+}
+
+func (s *Server) Stop(ctx context.Context) {
+	if err := s.srv.Shutdown(ctx); err != nil {
+		s.log.Fatal("Server forced to shutdown: ", err)
+	}
+
+	os.Exit(1)
+}
