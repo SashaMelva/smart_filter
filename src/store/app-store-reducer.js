b69671769ea599cdc7220addb4168f8247204623
Max <62894648+MaxSemukhin@users.noreply.github.com>
2024-05-19 03:49:45 -0400

    Update app-store-reducer.js

--- src/store/app-store-reducer.js
+++ src/store/app-store-reducer.js
@@ -1,23 +1,11 @@
 import {initialState} from "./initialState";
-import {SET_ME, SET_CREDITS, SET_ME_FETCHED, SET_CREDITS_FETCHED} from "./action_types";
+import {SET_ME} from "./action_types";
 
 const appPageReducer = (state = initialState.appStore, action) => {
     let stateCopy = {...state};
     switch (action.type) {
         case SET_ME:
             stateCopy.me = action.data;
-            return stateCopy;
-            break;
-        case SET_CREDITS:
-            stateCopy.credits = action.data;
-            return stateCopy;
-            break;
-        case SET_ME_FETCHED:
-            stateCopy.meFetched = action.data;
-            return stateCopy;
-            break;
-        case SET_CREDITS_FETCHED:
-            stateCopy.creditsFetched = action.data;
             return stateCopy;
             break;
         default:
